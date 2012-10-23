d550eb7733ec3d047f3b26b8e02496cec22a2fb3
Camilo Peric de Freitas <camiloperic@voador.(none)>
2012-10-23 20:21:43 -0200

    New tables defined in create.sql

--- doc/create.sql
+++ doc/create.sql
@@ -1,8 +1,33 @@
 CREATE TABLE citizen (
-	id SERIAL PRIMARY KEY,
-	te VARCHAR(20) UNIQUE,
-	name VARCHAR(120),
-	email VARCHAR(50),
-	active INTEGER,
-	passwd VARCHAR(32)
+	id 				SERIAL 			PRIMARY KEY,
+	te 				VARCHAR(20) 	UNIQUE,
+	name 			VARCHAR(120),
+	email 			VARCHAR(50),
+	active 			INTEGER,
+	passwd 			VARCHAR(32)
+);
+
+CREATE TABLE project (
+	id 				SERIAL 			PRIMARY KEY,
+	citizen_id 		INTEGER 		REFERENCES citizen(id),
+	title 			VARCHAR(200),
+	abstract 		TEXT,
+	wording 		TEXT
+);
+
+CREATE TABLE vote (
+	id 				SERIAL 			PRIMARY KEY,
+	name 			TEXT 			UNIQUE
+);
+
+CREATE TABLE argument (
+	id 				SERIAL 			PRIMARY KEY,
+	vote_id			INTEGER 		REFERENCES vote(id),
+	wording 		TEXT
+);
+
+CREATE TABLE change (
+	id				SERIAL			PRIMARY KEY,
+	project_id		INTEGER			REFERENCES project(id),
+	new_project_id	INTEGER			REFERENCES project(id),
 );
